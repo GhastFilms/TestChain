8af4ebf37c1b2c417f103ef9d5750600049cf68f
GhastFilms <ghastfilms613@gmail.com>
2019-08-08 19:26:32 -0500

    finished merkel root function but its useless because it needs io read and clone

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
-extern crate ring;
 extern crate bigint;
-use ring::digest::{SHA256, Digest, Context};
+extern crate ring;
 use bigint::uint::U256;
+use ring::digest::{digest, Algorithm, Context, Digest, SHA256};
 use std::{boxed::Box, io::Read};
+
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
 
 struct Block {
     Data: BlockData,
@@ -11,46 +14,57 @@ struct Block {
     MagicNum: u32,
     Size: u32,
 }
-
+/*
 impl Block {
     pub fn new() -> Self {
         Block {
-            data: vec!(String::new("block"), String::new("block"), String::new("block"), String::new("block"), String::new("block"), ),
+
+            Data: vec!(String::new("block"), String::new("block"), String::new("block"), String::new("block"), String::new("block")),
         }
     }
 }
+*/
 
 struct BlockData {
-    data: Vec<dyn Read>,
+    data: Vec<Box<dyn Read>>,
 }
 
 struct UnminedBlockHeader {
-    blockref: Box<&'a Block>,
+    //blockref: Box<&'a Block>,
     version: u32,
-    prev_hash: U256,
+    prev_hash: Digest,
     target: u32,
-    merkle_root: U256,
+    merkle_root: Option<Digest>,
     time: u32,
 }
-
-
-
 
 struct MinedBlockHeader {
     version: u32,
-    prev_hash: U256,
+    prev_hash: Digest,
     target: u32,
-    merkle_root: U256,
+    merkle_root: Digest,
     time: u32,
 }
 
-
-
-
-fn merkel_root(i: Vec<dyn Read>) -> U256 {
-    if i.len() = 1 {
-        digest::digest(&digest::SHA256, i.get(0).unwrap().bytes())
+pub fn merkle_root<T: Hash + Clone>(mut i: Vec<T>) -> Result<u64, ()> {
+    if i.is_empty() {
+        println!("no values provided");
+        return Err(());
     }
-    if i.len().is_even() {
-
+    if i.len() == 1 {
+        Ok(calculate_hash(&i.get(0)))
+    } else {
+        let w: usize = (i.len() as f64 / 2.0).floor() as usize;
+        let snd: Vec<T> = i.split_off(w);
+        let mut combined: _ = merkel_root(i).unwrap().to_string();
+        combined.push_str(&(merkel_root(snd).unwrap().to_string()));
+        Ok(calculate_hash(&combined))
     }
+}
+
+
+fn calculate_hash<T: Hash>(t: &T) -> u64 {
+    let mut s = DefaultHasher::new();
+    t.hash(&mut s);
+    s.finish()
+}
